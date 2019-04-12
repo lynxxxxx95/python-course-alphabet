0352b5be20f1eb46e914ced401a33537f3bf0289
dvasilov <dvasilov@echoua.com>
2019-04-12 17:58:17 +0100

    lesson

--- build_in_methods_iterators/lesson/dict_comprehension.py
+++ build_in_methods_iterators/lesson/dict_comprehension.py
@@ -4,8 +4,11 @@ data = {k: v for v, k in enumerate(string.ascii_lowercase, start=1)}
 
 data_keys = [f"Key is : {x}; Value is {y}" for x, y in enumerate(data.items())]
 data_keys_1 = [f"Key is : {x}; Value is {y}" for x, y in data.items()]
-data_keys_2 = [f"Key is : {x}; Value is {y}" for x, y, c in enumerate(data.items())]
+# this will fail
+# data_keys_2 = [f"Key is : {x}; Value is {y}" for x, y, c in enumerate(data.items())]
 
-print(data_keys)
-print(type(data))
+data = {k: v for v, k in enumerate(string.ascii_lowercase, start=1)}
+dict_if_syntax = {k: v for v, k in enumerate(string.ascii_lowercase, start=1) if v % 2 == 0}
+
 print(data)
+print(dict_if_syntax)

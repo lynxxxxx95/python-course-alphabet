63143217b7a2d2fb403739103dea04ee38a097ca
dvasilov <dvasilov@echoua.com>
2019-04-12 17:34:49 +0100

    lesson

--- build_in_methods_iterators/lesson/list_comprehension.py
+++ build_in_methods_iterators/lesson/list_comprehension.py
@@ -3,8 +3,11 @@ import string
 list_1 = [x for x in string.ascii_lowercase]
 list_2 = [1 for x in string.ascii_lowercase]
 
-list_3 = list(range(0, 100, 2))
+list_3 = [x for x in range(0, 100) if x % 2 == 0]
+
+list_4 = [x if x % 2 == 0 else -1 for x in range(0, 100)]
 
 print(list_1)
 print(list_2)
 print(list_3)
+print(list_4)

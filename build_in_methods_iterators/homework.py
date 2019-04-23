db07820c30621e55178ad0de33709ebf02a2c031
gorbanskyy <andrey.gorbanskiy@gmail.com>
2019-04-23 15:40:34 +0300

    improvement

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -17,12 +17,11 @@ def task_1_fix_names_start_letter(data: DT) -> DT:
         >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
     """
 
-    for v_dict in data:
-        try:
-            v_dict['name'] = v_dict['name'].title()
-        except KeyError:
-            continue
-    return data
+    def make_title(p_dict: dict, p_key: list) -> dict:
+        if p_key in p_dict:
+            p_dict[p_key] = p_dict[p_key].title()
+        return p_dict
+    return list(map(make_title, data, ['name']*len(data)))
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
@@ -34,10 +33,11 @@ def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
         >>> [{'name': 'Alex'}, {'name': 'denys'}]
     """
 
-    for counter in range(len(data)):
-        for key in redundant_keys:
-            del data[counter][key]
-    return data
+    def del_key(p_dict: dict) -> dict:
+        for v_key in redundant_keys:
+            del p_dict[v_key]
+        return p_dict
+    return list(map(del_key, data))
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
@@ -64,14 +64,7 @@ def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     Find the shortest string
     """
 
-    v_result = None
-    for v_item in data:
-        if v_result is None:
-            v_result = str(v_item)
-            continue
-        if len(str(v_item)) < len(v_result):
-            v_result = str(v_item)
-    return v_result
+    return min(map(str, data), key = len, default=None)
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
@@ -81,16 +74,7 @@ def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
 
     """
 
-    v_data = [0, 0]
-    flag = False
-    for v_index, v_dict in enumerate(data):
-        try:
-            if v_dict[key] < v_data[1] or not flag:
-                v_data[0], v_data[1] = v_index, v_dict[key]
-                flag = True
-        except KeyError:
-            continue
-    return data[v_data[0]]
+    return min(filter(lambda v_dict: key in v_dict, data), key=lambda v_dict: v_dict[key])
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
@@ -98,13 +82,7 @@ def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     Find max value from list of lists
     """
 
-    v_data = []
-    for v_list in data:
-        if len(v_list):
-            v_list.sort(reverse=True)
-            v_data.append(v_list[0])
-    v_data.sort(reverse=True)
-    return v_data[0]
+    return max([v_num for v_list in data for v_num in v_list], default=None)
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
@@ -145,18 +123,16 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
     """
 
     def is_prime(value):
-        if value == 1:
-            return False
-        for i in range(2, value):
+        for i in range(3, value, 2):
             if value % i == 0:
                 return False
         return True
 
-    v_out = 1
-    while v_out <= 200:
+    yield 2
+    for i in range(3, 200, 2):
+        v_out = i
         if is_prime(v_out):
             yield v_out
-        v_out += 1
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
